2c53c2f34f5bc51621ec4ad28186e033d31fb60d
Christian Kapik <christian.kapik@allianz.de>
2017-02-09 17:05:33 +0100

    added parameter for port and container name to run script and shared folder 'indocker' to add html files to apaches /var/www/html/fromhost directory

--- run.sh
+++ run.sh
@@ -1,3 +1,22 @@
 #!/bin/bash  
 
-docker run --rm --name=apache2 -it -p 80:80 ckapik/apache2 
+export shared_folder="$(pwd)/indocker"
+
+export container_port=80
+export container_name=apache2
+
+
+if [ ! -z "$1" ] ; then
+	container_port=$1
+fi
+if [ ! -z "$2" ] ; then
+	container_name=$2
+fi
+
+
+
+echo "starting docker container with following command line:"
+echo "docker run -v $shared_folder:/var/www/html/fromhost --rm --name=$container_name -it -p $container_port:80 ckapik/apache2"
+echo "open http://localhost:$container_port/fromhost/index.html after the container is started"
+
+docker run -v $shared_folder:/var/www/html/fromhost --rm --name=$container_name -it -p $container_port:80 ckapik/apache2
